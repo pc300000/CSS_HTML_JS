ff2b11f0c0ffbd43052e6e0667f71997235ba910
Chandran Palanisamy <pch300@gmail.com>
2024-05-16 20:11:38 -0400

    Cosmetic update
    Cosmetic update -no functional impact.

--- Module_5_solution/js/script.js
+++ Module_5_solution/js/script.js
@@ -30,8 +30,7 @@ $(function () {
     /*##"https://coursera-jhu-default-rtdb.firebaseio.com/categories.json"; */
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoryHtml = "snippets/category-snippet.html";
-  var menuItemsUrl =
-    "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+  var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
   var menuItemsTitleHtml = "snippets/menu-items-title.html";
   var menuItemHtml = "snippets/menu-item.html";
 
